bb25c8e08c9fd3fb8a63d371295da44af5f99185
Hugo Josefson <hugo@josefson.org>
2024-06-12 01:12:44 +0200

    fix: truncate if not appending

--- src/sponge.ts
+++ src/sponge.ts
@@ -49,7 +49,7 @@ async function resolveOutput(
   append: boolean,
 ): Promise<Writer | (Writer & Closer)> {
   if (isStringOrURL(output)) {
-    return await Deno.open(output, { write: true, append });
+    return await Deno.open(output, { write: true, append, truncate: !append });
   }
   return output;
 }
